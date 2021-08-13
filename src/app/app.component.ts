4289803fed389a176072188ed3b85cc3d99c388d
fazt <fazt@faztweb.com>
2021-08-12 20:46:48 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Coin {
+  id: string;
+  image: string;
+  name: string;
+  symbol: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+  total_volume: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-coingecko-api';
+export class AppComponent implements OnInit {
+  api: string =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+  coins: Coin[] = [];
+  titles: string[] = ['#', 'Coin', 'Price', 'Price Change', '24H Volume'];
+  searchText: string = '';
+  filteredCoints: Coin[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Coin[]>(this.api).subscribe(
+      (res) => {
+        this.coins = res;
+        this.filteredCoints = this.coins;
+      },
+      (err) => console.error(err)
+    );
+  }
+
+  searchCoin() {
+    this.filteredCoints = this.coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
 }
